aecc86ca07077cf92866ba36614e4b3e096e5e95
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2014-04-22 19:30:50 -0500

    Add news changes

---
+++ basico_9.php
@@ -0,0 +1,19 @@
+<?php
+print sumar(14, 16);
+
+/**
+ * Suma dos valores ingresados.
+ * 
+ * @param $valor_a
+ *   Valor a por ingresar.
+ * @param $valor_b
+ *   Valor b por ingresar.
+ * 
+ * @return int
+ *   La suma de dos números enteros.
+ */
+function sumar($valor_a, $valor_b) {
+  return $valor_a + $valor_b;
+}
+
+?>
